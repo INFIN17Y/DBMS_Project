072c86b1972771341ffe46f7191101e92917fb7a
INFIN17Y <kavinduanuhas398@gmail.com>
2024-11-03 15:22:39 +0530

    insert medical data

--- table.sql
+++ table.sql
@@ -11,3 +11,10 @@ CREATE TABLE
         FOREIGN KEY(DepID) REFERENCES Department(DepID),
         PRIMARY KEY(MeidcalID));
 
+
+INSERT INTO MEDICAL VALUES
+("MED001","TG/2021/1014","ICT1233","DpICT","2023-07-20","Medical certificate for flu"),
+("MED002","TG/2021/1036","ICT1233","DpICT","2023-05-21","Sick leave request"),
+("MED003","TG/2021/1011","ICT1212","DpICT","2023-05-11","Doctor s note for a medical appointment"),
+("MED004","TG/2021/1020","ICT1212","DpICT","2023-03-29","Medical leave due to injury");
+
